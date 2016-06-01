5318fb54bed4cd97f6a41c8a3d8d28abd24d8bbb
Matt Ross <matthewtross@yahoo.com>
2016-05-31 23:14:34 -0500

    settings updates

--- mysite/settings.py
+++ mysite/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_ROOT, 'static'),
+    
 )
 
 # Simplified static file serving.
